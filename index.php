c6e11d9b221e0f475b06c5ec22f5a2b3df7d2839
Seyi Onifade <xyluz@ymail.com>
2020-03-31 17:13:00 +0100

    Final work with styling

--- index.php
+++ index.php
@@ -2,9 +2,16 @@
     <p>
         <?php  print_alert(); ?>
     </p>
-    Welcome to SNG: Hospital for the ignorant <br /><hr />
-    <p>This is a specialist hospital to cure ignorance!</p>
-    <p>Come as you are, it is completely free!</p>
+
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+        <h1 class="display-4"> Welcome to SNG: Hospital for the ignorant</h1>
+        <p class="lead">This is a specialist hospital to cure ignorance!</p>
+        <p class="lead">Come as you are, it is completely free!</p>
+        <p>
+            <a class="btn btn-bg btn-outline-secondary" href="login.php">Login</a>
+            <a class="btn btn-bg btn-outline-primary" href="register.php">Register</a>            
+        </p>
+    </div>
 
 
 <?php include_once('lib/footer.php'); ?>
--- lib/header.php
+++ lib/header.php
@@ -5,5 +5,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to SNG : Hospital for the ignorant</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="js/scripts.js"></script>
 </head>
 <body>
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+        <h5 class="my-0 mr-md-auto font-weight-normal"><a href="index.php">StartNG Hospital</a></h5>
+        <nav class="my-2 my-md-0 mr-md-3">
+            <a class="p-2 text-dark" href="index.php">Home</a>
+            <?php if(!isset($_SESSION['loggedIn'])){ ?>
+        
+                <a class="p-2 text-dark" href="login.php">Login</a> 
+                <a class="btn btn-primary" href="register.php">Register</a> 
+                <!-- <a class="p-2 text-dark" href="forgot.php">Forgot Password</a> -->
+            <?php }else{ ?>
+                
+                <a class="p-2 text-dark" href="dashboard.php">Dashboard</a>                
+                <a class="p-2 text-dark" href="reset.php">Reset Password</a>
+                <a class="p-2 text-dark" href="logout.php">Logout</a>
+            <?php } ?>
+          
+        </nav>
+       
+    </div>
